bc60f49ecf3715321ca3f5c48264a73b1c252cbb
Jun Shimura <junshimura@gmail.com>
2022-11-22 17:33:53 +0900

    ClassTest

--- Assets/ClickAction.cs
+++ Assets/ClickAction.cs
@@ -18,6 +18,22 @@ public class ClickAction : MonoBehaviour, IPointerClickHandler
     }
     public void OnPointerClick(PointerEventData eventData)
     {
+        Vector3 vTemp = new Vector3(1, 2, 3);
+        vTemp = force;
+        vTemp.x = -5;
+        CL clA = new CL(1, 2);
+        CL clB = new CL(3, 5);
+        clB = clA;
+        clB.x = -5;
         rigidBody.AddForce(force, forceMode);
     }
 }
+class CL
+{
+    public float x; public float y;
+    public CL(float x, float y)
+    {
+        this.x= x; this.y = y;    
+    }
+};
+
